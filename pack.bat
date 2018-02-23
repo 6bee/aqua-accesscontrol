94a10d93998f9005bd4702bdea7327d76343f3a6
6bee <christof.senn@gmail.com>
2018-02-24 00:02:11 +0100

    0.1.0-alpha-002

--- pack.bat
+++ pack.bat
@@ -1,9 +1,10 @@
 @echo off
 set configuration=Debug
-set version-suffix="alpha-001"
+set version-suffix="alpha-002"
 clean ^
   && dotnet restore ^
-  && dotnet build src\Aqua.AccessControl --configuration %configuration% ^
-  && dotnet build test\Aqua.AccessControl.Tests --configuration %configuration% ^
   && dotnet test test\Aqua.AccessControl.Tests\Aqua.AccessControl.Tests.csproj --configuration %configuration% ^
+  && dotnet test test\Aqua.AccessControl.Tests.SQLite.EF6\Aqua.AccessControl.Tests.SQLite.EF6.csproj --configuration %configuration% ^
+  && dotnet test test\Aqua.AccessControl.Tests.SQLite.EFCore\Aqua.AccessControl.Tests.SQLite.EFCore.csproj --configuration %configuration% ^
+  && dotnet test test\Aqua.AccessControl.Tests.SqlServer.EFCore\Aqua.AccessControl.Tests.SqlServer.EFCore.csproj --configuration %configuration% ^
   && dotnet pack src\Aqua.AccessControl\Aqua.AccessControl.csproj --output "..\..\artifacts" --configuration %configuration% --include-symbols --version-suffix "%version-suffix%"
