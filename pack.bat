55c915acdf7b1e375335238f3f08154431b1fdb4
Christof Senn <christof.senn@gmail.com>
2020-12-29 18:41:51 +0100

    include git commit hash in assembly and package info

--- pack.bat
+++ pack.bat
@@ -5,4 +5,4 @@ clean ^
   && dotnet test test\Aqua.AccessControl.Tests.SQLite.EF6       --configuration %configuration% ^
   && dotnet test test\Aqua.AccessControl.Tests.SQLite.EFCore    --configuration %configuration% ^
   && dotnet test test\Aqua.AccessControl.Tests.SqlServer.EFCore --configuration %configuration% ^
-  && dotnet pack src\Aqua.AccessControl                         --configuration %configuration% --include-symbols --include-source --output "artifacts" 
+  && dotnet pack src\Aqua.AccessControl                         --configuration %configuration%
