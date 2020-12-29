2027c05b1e1e1ca8c392e7b09252c4e3f520f523
Christof Senn <christof.senn@gmail.com>
2020-12-29 17:37:39 +0100

    updated project files

--- pack.bat
+++ pack.bat
@@ -1,10 +1,8 @@
 @echo off
 set configuration=Debug
-set version-suffix="alpha-002"
 clean ^
-  && dotnet restore ^
-  && dotnet test test\Aqua.AccessControl.Tests\Aqua.AccessControl.Tests.csproj --configuration %configuration% ^
-  && dotnet test test\Aqua.AccessControl.Tests.SQLite.EF6\Aqua.AccessControl.Tests.SQLite.EF6.csproj --configuration %configuration% ^
-  && dotnet test test\Aqua.AccessControl.Tests.SQLite.EFCore\Aqua.AccessControl.Tests.SQLite.EFCore.csproj --configuration %configuration% ^
-  && dotnet test test\Aqua.AccessControl.Tests.SqlServer.EFCore\Aqua.AccessControl.Tests.SqlServer.EFCore.csproj --configuration %configuration% ^
-  && dotnet pack src\Aqua.AccessControl\Aqua.AccessControl.csproj --output "..\..\artifacts" --configuration %configuration% --include-symbols --version-suffix "%version-suffix%"
+  && dotnet test test\Aqua.AccessControl.Tests                  --configuration %configuration% ^
+  && dotnet test test\Aqua.AccessControl.Tests.SQLite.EF6       --configuration %configuration% ^
+  && dotnet test test\Aqua.AccessControl.Tests.SQLite.EFCore    --configuration %configuration% ^
+  && dotnet test test\Aqua.AccessControl.Tests.SqlServer.EFCore --configuration %configuration% ^
+  && dotnet pack src\Aqua.AccessControl                         --configuration %configuration% --include-symbols --include-source --output "artifacts" 
