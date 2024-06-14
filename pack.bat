3b141a50c92771b20ebb167678687946fba9e959
Christof Senn <christof.senn@gmail.com>
2024-06-14 11:55:18 +0200

    remove batch file

--- pack.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set configuration=Debug
-clean ^
-  && dotnet test test\Aqua.AccessControl.Tests                  --configuration %configuration% ^
-  && dotnet test test\Aqua.AccessControl.Tests.SQLite.EF6       --configuration %configuration% ^
-  && dotnet test test\Aqua.AccessControl.Tests.SQLite.EFCore    --configuration %configuration% ^
-  && dotnet test test\Aqua.AccessControl.Tests.SqlServer.EFCore --configuration %configuration% ^
-  && dotnet pack src\Aqua.AccessControl                         --configuration %configuration%
